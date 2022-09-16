6f7804d40ee32f37097b5d793304eee0b07213b5
Marlene Barrera <103092395+marlenebarr@users.noreply.github.com>
2022-09-15 18:39:37 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import "./App.css";
+import RecipeCreate from "./RecipeCreate";
+import RecipeList from "./RecipeList";
+import RecipeData from "./RecipeData"
+
+function App() {
+  const [recipes, setRecipes] = useState(RecipeData);
+    const createRecipe = (newRecipe) =>   //Do you need those curly brackets? newRecipe is an object, but try both
+    setRecipes((currentRecipes) => [
+    ...currentRecipes,
+      newRecipe
+    ]);
+  const deleteRecipe = (indexToDelete) =>
+    setRecipes((currentRecipes) =>
+    currentRecipes.filter((recipe, index) => index !== indexToDelete)  //I am concerned that recipe is not read here...
+    );    
+
+  return (
+    <div className="App">
+      <header><h1>Delicious Food Recipes</h1></header>
+      <RecipeList
+        recipes={recipes}
+        deleteRecipe={deleteRecipe}     
+      />
+      <RecipeCreate 
+        createRecipe={createRecipe}
+      />
+    </div>
+  );
+}
+
+export default App;
